29123025d1218bca0eba36e2db1546c55c021df3
DouglasDart <douglas.r@marketsimplified.com>
2021-06-20 23:34:11 +0530

    GET and POST calls added

--- lib/network/flutter_utils_lib.dart
+++ lib/network/flutter_utils_lib.dart
@@ -1,5 +1,17 @@
 library flutter_utils_lib;
 
+import 'dart:convert';
+import 'package:http/http.dart';
+
 class HttpClient {
 
+  Future<Response> fetchGET(String getURL, {Map<String, String>? appendHeaders}) async {
+    final response = await get(Uri.parse(getURL), headers: appendHeaders);
+    return response;
+  }
+
+  Future<Response> fetchPOST(String postURL, {Map<String, String>? appendHeaders, Object? requestModel, Encoding? httpEncoding}) async {
+    final response = await post(Uri.parse(postURL), headers: appendHeaders, body: requestModel, encoding: httpEncoding);
+    return response;
+  }
 }
